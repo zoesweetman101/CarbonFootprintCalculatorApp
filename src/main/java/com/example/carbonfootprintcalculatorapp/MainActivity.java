46bc5103d504244dda0d1db6d47ad42b36861604
Zoe Sweetman <zoe.sweetman101@gmail.com>
2022-03-18 13:33:09 +0000

    Add files via upload

---
+++ src/main/java/com/example/carbonfootprintcalculatorapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.carbonfootprintcalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logIn(View view) {
+
+    }
+
+    public void register(View view) {
+        // Do something in response to button click
+    }
+}
